3bb3b5c0262c5328839534999b8e3afeb15e1a00
minani03 <94546403+minani03@users.noreply.github.com>
2025-02-26 09:46:28 -0500

    added Repositories

---
+++ src/main/java/edu/francis/my/sfupa/SQLite/Repository/LecturerRepository.java
@@ -0,0 +1,7 @@
+package edu.francis.my.sfupa.SQLite.Repository;
+
+import edu.francis.my.sfupa.SQLite.Models.Lecturer;
+import org.springframework.data.repository.CrudRepository;
+
+public interface LecturerRepository extends CrudRepository<Lecturer, Integer> {
+}
