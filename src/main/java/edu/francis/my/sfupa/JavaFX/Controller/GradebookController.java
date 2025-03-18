fd21a027871d5063eaed0416d31b433a56e87d7b
JoeyKing223 <joseph.king1389@proton.me>
2025-03-17 21:32:18 -0400

    broken!
    broken!

--- src/main/java/edu/francis/my/sfupa/JavaFX/Controller/GradebookController.java
+++ src/main/java/edu/francis/my/sfupa/JavaFX/Controller/GradebookController.java
@@ -1,12 +1,25 @@
 package edu.francis.my.sfupa.JavaFX.Controller;
 
-import edu.francis.my.sfupa.JavaFX.Controller.SceneUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.stage.Stage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import java.io.IOException;
 
 @Component
@@ -17,7 +30,7 @@ public class GradebookController {
 
     @FXML
     public void handleRunReporting(ActionEvent event) throws IOException {
-        SceneUtils.switchScene(event, "GradebookReport.fxml", springContext);
+        SceneUtils.switchScene(event, "GradeBookReport.fxml", springContext);
     }
 
     @FXML
@@ -31,12 +44,33 @@ public class GradebookController {
     }
 
     @FXML
-    public void handleBack(ActionEvent event) throws IOException {
-        SceneUtils.switchScene(event, "Gradebook.fxml", springContext);
+    public void handleBackMain(ActionEvent event) throws IOException {
+        SceneUtils.switchScene(event, "main-view.fxml", springContext);
+    }
+
+    // --- Menu Bar Actions ---
+    @FXML
+    private void handleExit() {
+        System.exit(0);
     }
 
     @FXML
-    public void handleBackMain(ActionEvent event) throws IOException {
-        SceneUtils.switchScene(event, "main-view.fxml", springContext);
+    public void handleAbout(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About SFU PA");
+        alert.setHeaderText("SFU PA Application");
+        alert.setContentText("This application is designed to manage gradebooks, instructor evaluations, and guest lecturers.");
+        alert.showAndWait();
+    }
+
+    // --- Sidebar Navigation ---
+    @FXML
+    public void handleGuestLecturer(ActionEvent event) throws IOException {
+        SceneUtils.switchScene(event, "GuestLecturer.fxml", springContext);
+    }
+
+    @FXML
+    public void handleInstructorEval(ActionEvent event) throws IOException {
+        SceneUtils.switchScene(event, "InstructorEvaluation.fxml", springContext);
     }
 }
--- src/main/java/edu/francis/my/sfupa/JavaFX/Controller/InstructorEvaluation.java
+++ src/main/java/edu/francis/my/sfupa/JavaFX/Controller/InstructorEvaluation.java
@@ -2,14 +2,10 @@ package edu.francis.my.sfupa.JavaFX.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import org.springframework.stereotype.Component;
+import javafx.scene.control.Alert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
@@ -34,12 +30,38 @@ public class InstructorEvaluation {
     }
 
     @FXML
-    public void handleBack(ActionEvent event) throws IOException {
-        SceneUtils.switchScene(event, "InstructorEval.fxml", springContext);
+    public void handleBackMain(ActionEvent event) throws IOException {
+        SceneUtils.switchScene(event, "main-view.fxml", springContext);
+    }
+
+    // --- Menu Bar Actions ---
+    @FXML
+    private void handleExit() {
+        System.exit(0);
     }
 
     @FXML
-    public void handleBackMain(ActionEvent event) throws IOException {
-        SceneUtils.switchScene(event, "main-view.fxml", springContext);
+    public void handleAbout(ActionEvent event) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About SFU PA");
+        alert.setHeaderText("SFU PA Application");
+        alert.setContentText("This application is designed to manage gradebooks, instructor evaluations, and guest lecturers.");
+        alert.showAndWait();
+    }
+
+    // --- Sidebar Navigation ---
+    @FXML
+    public void handleGuestLecturer(ActionEvent event) throws IOException {
+        SceneUtils.switchScene(event, "GuestLecturer.fxml", springContext);
+    }
+
+    @FXML
+    public void handleGradebook(ActionEvent event) throws IOException {
+        SceneUtils.switchScene(event, "Gradebook.fxml", springContext);
+    }
+
+    @FXML
+    public void handleInstructorEval(ActionEvent event) throws IOException {
+        SceneUtils.switchScene(event, "InstructorEvaluation.fxml", springContext);
     }
 }
