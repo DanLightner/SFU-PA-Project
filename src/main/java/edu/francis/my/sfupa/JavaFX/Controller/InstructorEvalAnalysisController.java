e1d7b6c9ebbb46ed6e6d7795888d35dd9cc83fd2
JoeyKing223 <joseph.king1389@proton.me>
2025-03-28 09:41:11 -0400

    analyze11
    hh

--- src/main/java/edu/francis/my/sfupa/JavaFX/Controller/InstructorEvalAnalysisController.java
+++ src/main/java/edu/francis/my/sfupa/JavaFX/Controller/InstructorEvalAnalysisController.java
@@ -88,7 +88,29 @@ public class InstructorEvalAnalysisController {
         }
         if (courseCmb != null) {
             courseCmb.setItems(FXCollections.observableArrayList(
-                    "Health 101", "Health Science 213", "Heart Studies 340", "CS101"
+                    "CS101", "PA 400",
+                    "PA 401",
+                    "PA 402",
+                    "PA 403",
+                    "PA 404",
+                    "PA 405",
+                    "PA 406",
+                    "PA 420",
+                    "PA 421",
+                    "PA 422",
+                    "PA 423",
+                    "PA 424",
+                    "PA 425",
+                    "PA 426",
+                    "PA 427",
+                    "PA 428",
+                    "PA 429",
+                    "PA 430",
+                    "PA 431",
+                    "PA 432",
+                    "PA 451",
+                    "PA 452",
+                    "PA 453"
             ));
         }
 
