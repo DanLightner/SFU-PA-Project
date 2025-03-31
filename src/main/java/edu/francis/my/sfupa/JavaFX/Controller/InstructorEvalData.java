46f0b935263c027ab6d4bc340e3b9a0d749282d3
Dan Lightner <DanLightner6@gmail.com>
2025-03-31 13:07:30 -0400

    vibe coding demon

---
+++ src/main/java/edu/francis/my/sfupa/JavaFX/Controller/InstructorEvalData.java
@@ -0,0 +1,30 @@
+package edu.francis.my.sfupa.JavaFX.Controller;
+
+import edu.francis.my.sfupa.SQLite.Models.SemesterName;
+
+public class InstructorEvalData {
+    private String courseId;
+    private String courseName;
+    private String semester;
+    private String year;
+
+    public InstructorEvalData(String courseId, String courseName, SemesterName semester, String year) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.semester = semester.toString();
+        this.year = year;
+    }
+
+    // Getters and setters
+    public String getCourseId() { return courseId; }
+    public void setCourseId(String courseId) { this.courseId = courseId; }
+    
+    public String getCourseName() { return courseName; }
+    public void setCourseName(String courseName) { this.courseName = courseName; }
+    
+    public String getSemester() { return semester; }
+    public void setSemester(String semester) { this.semester = semester; }
+    
+    public String getYear() { return year; }
+    public void setYear(String year) { this.year = year; }
+} 
